0011fbe2b8ad2808cde908b5ab2c5d407e8d9615
pablo <paperezpablo@gmail.com>
2023-08-30 01:28:20 -0300

    MetodoTasa

--- procesarOperaciones/tarjetaCred.java
+++ procesarOperaciones/tarjetaCred.java
@@ -1,6 +1,7 @@
 package procesarOperaciones;
 
-// punto 1 de mostrar info
+// puntos informes, validaciones e indentificar
+
 public class tarjetaCred {
     
     private String PAN;
@@ -16,9 +17,13 @@ public class tarjetaCred {
         this.fechaVencimiento = fechaVencimiento;
         this.PAN = PAN;
         this.tipo = tipo;
-
     }  
     
+// Se crea otro metodo para validar si son distintas o no las tarjetas
+    public tarjetaCred(String string, String string2) {
+
+    }
+
     // Para obtener la informacion de la tarjet
     public String obtenerInfoTarjeta() {
         return "Numero de tarjeta: " + PAN + " nombre: " + titular + " Fecha de vencimiento " +  fechaVencimiento + " saldo "+ saldo;
@@ -36,10 +41,27 @@ public class tarjetaCred {
         return false;
     }
 
+    // probamos si el metodo funciona correctamente 
 
+    public static void main(String[] args){
+        tarjetaCred tarjeta1 = new tarjetaCred("visa", "1231654653");
+        tarjetaCred tarjeta2 = new tarjetaCred("nara", "1231654653");
+        tarjetaCred tarjeta3 = new tarjetaCred("amex", "1231654653");
 
+        if ( tarjeta1.distinto(tarjeta2)) {
+            System.out.println("La tarjea 1 es distina a la tarjeta 2");
+        }else{
+            System.out.println("La tarjea 1 es igual a la tarjeta  2");
+        }
 
-    // Probar si esta bien el metodo
+        if ( tarjeta1.distinto(tarjeta3)) {
+              System.out.println("La tarjea 1 es distina a la tarjeta 3");
+        }else{
+            System.out.println("La tarjea 1 es igual a la tarjeta  ");
+        }
+
+    }
+     // Probar si esta bien el metodo
     // public static void main(String[] args){
     //     tarjetaCred mitarjeta = new tarjetaCred("123123123123", "PAblo", "06/05", 1000);
     //     String info = mitarjeta.obtenerInfoTarjeta();
